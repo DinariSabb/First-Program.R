197646a1aa5017f4f1f29836a1101967be6c3882
DinariSabb <111791664+DinariSabb@users.noreply.github.com>
2022-09-15 00:35:41 -0400

    Add files via upload

---
+++ Sep-2.R
@@ -0,0 +1,14 @@
+#Author: Dinari Sabb, Date: 09/10/22, 
+
+#Reading the data from csv file
+ setwd("/Users/dinar/Downloads")
+
+data <- read.csv(file = 'COVID-Data.csv', header=TRUE)
+
+head(data)
+
+# Correlation analysis between confirmed cases and number of deaths
+# Check if there is any relationship between number of deaths and confirmed cases
+
+
+
